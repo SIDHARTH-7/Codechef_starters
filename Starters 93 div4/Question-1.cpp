5221d4c99c5126584cfad25b8ccfb051ee4e4b8c
Sidharth Mishra <106550602+SIDHARTH-7@users.noreply.github.com>
2023-06-07 23:45:24 +0530

    7 jun 23

---
+++ Starters 93 div4/Question-1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,x;
+	    cin>>n>>x;
+	    if(n>x)
+	        cout<<"NO"<<endl;
+        else
+            cout<<"YES"<<endl;
+	}
+	return 0;
+}

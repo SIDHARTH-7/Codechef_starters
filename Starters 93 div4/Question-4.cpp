6ad77435b838f3cd591ed877cd683f0534985f97
Sidharth Mishra <106550602+SIDHARTH-7@users.noreply.github.com>
2023-06-07 23:48:48 +0530

    7 jun 23

---
+++ Starters 93 div4/Question-4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n,p=0;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>arr[i];
+	        if(arr[i]%2==0)
+	            p++;
+	    }
+	    int pro=1;
+        for(int i=0;i<p;i++)
+        {
+            pro%=1000000007;
+            pro*=2;
+        }
+        cout<<pro<<endl;
+	}
+	return 0;
+}
